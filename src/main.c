99ae0570f073e969ee1871d6dd62696f2b4a4a74
Stefano Bertoli <elkiwydev@gmail.com>
2020-02-15 12:14:25 +0100

    Formatting

--- src/main.c
+++ src/main.c
@@ -14,10 +14,9 @@
 
 
 char* html_head = "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><meta name='author' content='Stefano Bertoli'><link rel='stylesheet' type='text/css' href='../links/main.css'><title>ElKiwyArt</title></head><body>";
-
 char* html_header = "<header><a id='logo' href='home.html'>ElKiwyArt</a></header>";
-
 char *html_footer = "FOOTER</body></html>";
+
 
 
 typedef struct Page{
