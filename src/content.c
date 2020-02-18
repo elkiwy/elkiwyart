696c31f083d0a0f05b2b87bfbf94eb1d9ebca673
Stefano Bertoli <elkiwydev@gmail.com>
2020-02-19 00:09:30 +0100

    Added more content

--- src/content.c
+++ src/content.c
@@ -6,26 +6,57 @@
 #define SENDTEXT(page, text) "<a href='" page ".html'>" text "</a>"
 
 #define IMAGE(name) "<img src='../media/identity/" name "'/>"
+#define PAR(content) "<p>" content "</p>"
 
 
 
-
+////////////////////////////
+// Home
+////////////////////////////
 Page* home = create_page(NULL, "home");
 add_paragraph(home, "Welcome to ElKiwyArt website!");
-add_paragraph(home, "This is my very personal attempt to show who am I, and what I do. Since I do many different things sprinkled around the internet, the scope of this website is trying to group them all up and present everything to you in a nice and simple way.");
-add_paragraph(home, "This is not a simple portfolio where i present everything I made or released. There will also be random posts on anything I do in a particular time in my life, maybe some big project, or maybe just a little dumb study on something." );
-add_paragraph(home, "");
+add_paragraph(home, "This is my very personal attempt to show who am I, and what I do. Since I do many different things sprinkled around the internet, the scope of this website is to try group all up and present everything to you in a nice and clear way.");
+add_paragraph(home, "But, this is not a portfolio where i present everything I made or released. There will also be random posts on anything I do in a particular time in my life, maybe some big project, or maybe just a little dumb study on something." );
 
 
 
+////////////////////////////
+// Art
+////////////////////////////
+Page* art = create_page(home, "art");
 
-create_page(home, "blog");
-create_page(home, "art");
+Page* scorci = create_page(art, "Scorci");
+add_preview_description(scorci, "Second study on procedurally generated trees synthesising a Japanese panorama.");
 
+Page* spring_flowers = create_page(art, "Spring_Flowers");
+add_preview_description(spring_flowers, "First study on procedurally generated trees.");
+
+Page* gates = create_page(art, "Gates");
+add_preview_description(gates, "Third study on procedurally generated trees, going for a more abstract and twisted shapes.");
+
+Page* deep_stars = create_page(art, "Deep_Stars");
+add_preview_description(deep_stars, "Studies on Quil blends and shapes.");
+
+Page* color_mountains = create_page(art, "Color_Mountains");
+add_preview_description(color_mountains, "Part of my earliest studies on Quil blends.");
+
+
+////////////////////////////
+// Programs
+////////////////////////////
 Page* programs = create_page(home, "programs");
-create_page(programs, "Gisp");
-create_page(programs, "PaYnter");
 
+Page* gisp = create_page(programs, "Gisp");
+add_preview_description(gisp, "A Graphic-oriented lisp dialect written in C and Cairo lib for drawing.");
+
+Page* paynter = create_page(programs, "PaYnter");
+add_preview_description(paynter, "A Python module to procedurally generate images using tools that emulates image editing software tools.");
+
+
+
+////////////////////////////
+// Games
+////////////////////////////
 Page* games = create_page(home, "games");
 create_page(games, "BeardedBear");
 create_page(games, "BuildTutto");
