cb65a499b5b1d615d0b7a6bc75e518f79988204b
Stefano Bertoli <elkiwydev@gmail.com>
2020-02-20 21:38:29 +0100

    Added more content

--- src/content.c
+++ src/content.c
@@ -34,13 +34,37 @@ add_preview_description(gates, "Third study on procedurally generated trees, goi
 Page* scorci = create_page(art, "Scorci");
 add_preview_description(scorci, "Second study on procedurally generated trees synthesising a Japanese panorama.");
 
-Page* spring_flowers = create_page(art, "Spring_Flowers");
+Page* spring_flowers = create_page(art, "Spring Flowers");
 add_preview_description(spring_flowers, "First study on procedurally generated trees.");
+add_paragraph(spring_flowers, "It's Spring time! This is by far my favorite season of the year both for the warm clean air and for the beautiful show that trees create for us with their flowers. ");
+add_paragraph(spring_flowers, "The idea for this series came by watching a tree right in front of my house. After few times passing beside it each day going to work, I thought that these branches and flowers could be recreated by some code and functions in Clojure.");
+add_header(spring_flowers, "The style", 2);
+add_paragraph(spring_flowers, "Once the idea was there, it was time to choose a way to represent that object into the canvas. This wasn't a hard choice to make, since as soon as I decided to try draw some trees, it instantly come to my mind the Japanese woodblock prints with their simplicity, style, and colors.");
+add_header(spring_flowers, "The process", 2);
+add_paragraph(spring_flowers, "This series started with some path studies. The path structure present inside generative-toolbelt was a nice start, but a simple disposed line wasn't good enough to emulate the flow and curves of the tree's branches. ");
+add_image(spring_flowers, "default_1080.png");
+add_paragraph(spring_flowers, "After some rework on the path generation, a decent twig-like path was ready to grow to a larger tree. The next thing was to make the branch split into smaller sub-branches in certain point of the path. After some troubles calculating the right angle to make the minor branches, the main branch generator function was ready to be reused many more times.");
+add_image(spring_flowers, "default_1080.png");
+add_image(spring_flowers, "default_1080.png");
+add_image(spring_flowers, "default_1080.png");
+add_paragraph(spring_flowers, "Once this was done, it was time for a lot of parameter tweaking. Often you can get a totally different look and feel from some piece of code by simply changing few arguments, if the code is well made and parametric.");
+add_image(spring_flowers, "default_1080.png");
+add_image(spring_flowers, "default_1080.png");
+add_image(spring_flowers, "default_1080.png");
+add_image(spring_flowers, "default_1080.png");
+add_paragraph(spring_flowers, "Last but not least, the touch of spring. This will transform those spooky and naked branches into a well dressed and colorful tree. The flowers has been realized with a rose function with n=5 and d=2. I choose a regular function to give the flowers a \"mathematical\" touch that contrast well with the realistic randomness of the branches. ");
+add_image(spring_flowers, "default_1080.png");
+add_image(spring_flowers, "default_1080.png");
+add_paragraph(spring_flowers, "With the flower in place, the base of the series generation was almost done. A few touches here and there to smooth out some rough edges and I was happy enough to create some high resolution images for the series. ");
+add_header(spring_flowers, "There is more...", 2);
+add_paragraph(spring_flowers, "I'm really happy with the result so far. I'm releasing this series as it is, but I feel that I can study this tree concept much more than this, so probably I'll keep working on this to expand and improve the style and composition of the artwork. So, if you liked this series, be sure to not miss the next one! ");
 
-Page* deep_stars = create_page(art, "Deep_Stars");
+
+
+Page* deep_stars = create_page(art, "Deep Stars");
 add_preview_description(deep_stars, "Studies on Quil blends and shapes.");
 
-Page* color_mountains = create_page(art, "Color_Mountains");
+Page* color_mountains = create_page(art, "Color Mountains");
 add_preview_description(color_mountains, "Part of my earliest studies on Quil blends.");
 
 
