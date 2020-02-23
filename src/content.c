906d5dbcbc5d85a89bc6385badcddc16f4369aee
Stefano Bertoli <elkiwydev@gmail.com>
2020-02-23 19:43:28 +0100

    Ported more content from old site

--- src/content.c
+++ src/content.c
@@ -30,12 +30,12 @@ add_stub(art, "Introduzione su ART");
 
 
 
+//TO REVIEW
 Page* gates = create_page(art, "Gates");
 add_preview_description(gates, "Third study on procedurally generated trees, going for a more abstract and twisted shapes.");
 add_paragraph(gates, "I really enjoyed the natural look of Scorci , but after that I started to want back some of the regular and geometric feel typical of generative art. So what's better than some natural-ish tree with weird geometric shapes? ");
 add_header(gates, "One more tree iteration", HEADER_SIZE_SMALL);
 add_paragraph(gates, "This may starts to feel a bit repetitive, but I wanted to get one more artwork with this style. The process to achieve this pieces has been pretty simple this time. I had to take the old tree function and tweak it to accept a path as input for the main trunk. Before these changes, that function was accepting just a single point and a starting direction for the tree to grow, but that had two major problems:");
-
 char* l1 = "The final structure of the tree was not predictable. Half of the times the tree would start in the right spot, then curving out of the canvas letting me with a bunch of nothing on the screen.";
 char* l2 = "I could not give the tree a fixed shape and then let the minor branches go where they wanted.";
 add_number_list(gates, 2, l1, l2);
@@ -43,8 +43,8 @@ add_image(gates, "start0.png");
 add_paragraph(gates, "After some changes I had something that could work for what I wanted to do:");
 add_image2(gates, "straight0.png", "straight1.png");
 add_paragraph(gates, "Once that the tree was following whatever the path I wanted, I had to find some nice shapes to draw.");
-add_image2(gates, "rectangles0.png", "rectangles1.png");
 add_image2(gates, "rectangle0.png", "rectangle1.png");
+add_image2(gates, "rectangles0.png", "rectangles1.png");
 add_paragraph(gates, "The concept was there, but the idea of trees floating mid-air with no roots to keep them anchored somewhere wasn't really right for this kind of artwork, so I had to add something to give some stability to the drawing.");
 add_paragraph(gates, "(Sadly I can't show this part of the process because I lost the images from that day)");
 add_paragraph(gates, "After some minor tweaks the artwork was ready to start the full resolution generation and be published.");
@@ -61,11 +61,38 @@ add_paragraph(gates, "I think that I'm done for the trees, and I would like to g
 
 
 
+//TO REVIEW
 Page* scorci = create_page(art, "Scorci");
 add_preview_description(scorci, "Second study on procedurally generated trees synthesising a Japanese panorama.");
-add_stub(scorci, "Migrare pagina");
+add_paragraph(scorci, "After an adeguate amount of time, here we are with another series in our hands. I'm so happy with the result of this one that I'd skip all the unnecessary intro words and jump right into the post!");
+add_header(scorci, "A rework of Spring Flowers", HEADER_SIZE_SMALL);
+add_paragraph(scorci, "To everyone that saw my previous posts, these new pieces could feel pretty similar to the series \"Spring Flowers\" since I reused the tree and flower structure and expanded that concept into a more complete tribute to the Ukiyo-E style Japanese woodprints.");
+add_paragraph(scorci, "The previous series was pretty minimalist and plain, but this new rework aimed to give the right mood to that concept by giving more context of the surrounding of that tree.");
+add_header(scorci, "Giving some space", HEADER_SIZE_SMALL);
+add_paragraph(scorci, "The first thing to do was adding a Bi-Linear gradient on top of everything that shades darker on the top and the bottom of the artwork, this gives a sense of space by giving a subtle hint of where the sky, ground, and horizon line is in the canvas.");
+add_paragraph(scorci, "This small detail alone already gave the artwork a totally different mood and feel.");
+add_image2(scorci, "gradient0.png", "gradient1.png");
+add_header(scorci, "Lighten up the image", HEADER_SIZE_SMALL);
+add_paragraph(scorci, "The images still felt a bit empty and lacking of details, so I took inspiration from some prints and I tried to see what all them had in common. One element that was almost always present is a big saturated circle representing the Sun (or the Moon, depending from the colors and interpretations).");
+add_paragraph(scorci, "After some experiments I found that drawing it pretty flat with only a small darker border around it was the more pleasing way to render it on the canvas, keeping in mind that the sky/ground gradient was still on top of it.");
+add_paragraph(scorci, "The color for the Sun/Moon is derived from the opposite of the sky/ground gradient hue to give the image a nice contrast and balance.");
+add_image3(scorci, "sun0.png", "sun1.png", "sun2.png");
+add_header(scorci, "Creating some stability", HEADER_SIZE_SMALL);
+add_paragraph(scorci, "Adding only a simple Sun/Moon wasn't enough to emulate the style of those woodblock prints, so I still wanted to add more recurring environmental elements. The mountains are another key protagonists in many Japanese artworks and panoramas. A big centered mountain also gave a lot of balance to the artworks by filling the space evenly.");
+add_paragraph(scorci, "So, to create the mountain shape I took advantage of my toolbelt functions for paths like the dispose and chaikin smoothing ones. Then the shape was filled by a gradient to blend it to the background.");
+add_image3(scorci, "mountain0.png", "mountain1.png", "mountain2.png");
+add_image(scorci, "mountain-sun-tree.png");
+add_header(scorci, "Signing the series", HEADER_SIZE_SMALL);
+add_paragraph(scorci, "To complete this work I decided to sign this series. Since I'm slowly studying Japanese in my free time, I wanted to keep following these Japan vibes and sign it using the Katakana alphabet (one of the three Japanese alphabet and the one used mainly for foreign words).");
+add_paragraph(scorci, "As I said in the intro, I'm really really happy with this results and I think that this is a good milestone for me and the style I'll use in my next works.");
 
 
+//"Scorci 137931.png"
+//"Scorci 991060.png"
+//"Scorci 606684.png"
+//"Scorci 313858.png"
+//"Scorci 263414.png"
+//"Scorci 214396.png"
 
 Page* spring_flowers = create_page(art, "Spring Flowers");
 add_preview_description(spring_flowers, "First study on procedurally generated trees.");
